6cdce097eadaaee15b944b214ddd00e08a775cb0
purbo75 <bsse1004@iit.du.ac.bd>
2023-02-11 01:28:12 +0600

    basic project structure is built

--- main.go
+++ main.go
@@ -9,9 +9,19 @@ func home(w http.ResponseWriter, r *http.Request){
 	w.Write([]byte("Hello from snippetbox"))
 }
 
+func snippetView(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+func snippetCrete(w http.ResponseWriter, r *http.Request){
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main(){
 	mux:= http.NewServeMux()
 	mux.HandleFunc("/",home)
+	mux.HandleFunc("/snippet/view",snippetView)
+	mux.HandleFunc("/snippet/create",snippetCrete)
 
 	log.Println("Starting Server on : 4000 ")
 	err:= http.ListenAndServe(":4000",mux)
