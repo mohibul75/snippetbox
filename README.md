c1aa7928f7bbbf65311e7043b1933dbf66b300dd
purbo75 <bsse1004@iit.du.ac.bd>
2023-02-17 15:03:17 +0600

    Updated Readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # snippetbox-go-project
+
+### Disabling Directory Listings
+FOllow this blog post (!Disabling Directory Listings)[https://www.alexedwards.net/blog/disable-http-fileserver-directory-listings]
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -14,7 +14,8 @@ func main() {
 	mux := http.NewServeMux()
 
 
-	fileServer:= http.FileServer(http.Dir("./ui/static/"))
+	fileServer:= http.FileServer(neuteredFileSystem{http.Dir("./ui/static/")})
+	mux.Handle("/static",http.NotFoundHandler())
 	mux.Handle("/static/", http.StripPrefix("/static",fileServer))
 
 	mux.HandleFunc("/", home)
