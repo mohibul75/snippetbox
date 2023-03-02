eb112dec12508018f42211258515748edf2870c1
purbo75 <bsse1004@iit.du.ac.bd>
2023-03-02 20:16:49 +0600

    DB connection pool added

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -54,14 +54,29 @@ func (app *application)snippetCrete(w http.ResponseWriter, r *http.Request){
 
 	if r.Method != http.MethodPost {
 		w.Header().Set("Allow","POST")
+
 		// w.WriteHeader(405)
 		//  w.Write([]byte("Method Not Allowed"))
-
 		// replacing above two commented lines with the below one
 
 		http.Error(w,"Method Not Allowed",http.StatusMethodNotAllowed)
 		return
 	}
 
-	w.Write([]byte("Create a new snippet..."))
+	//create one new entry
+
+	title:="0 snail"
+	content:= "0 Snail\nClimb Mount,\nBut Slow"
+	expires:=7
+
+	id, err:= app.snippet.Insert(title,content,expires)
+	if err!=nil{
+		//app.serverError(w,err)
+
+		return
+	}
+
+	// w.Write([]byte("Create a new snippet..."))
+
+	http.Redirect(w,r,fmt.Sprintf("/snippet/view?id=%d",id),http.StatusSeeOther)
 }
